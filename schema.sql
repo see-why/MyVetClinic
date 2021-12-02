cfc1d3f80310a4c2d2a4156c891d43399932cdcf
Iyadi Cyril <iyadicy@gmail.com>
2021-12-02 05:57:54 +0100

    Add schemas for new tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-
+DAY 1,
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
@@ -11,5 +11,37 @@ CREATE TABLE animals(
   weight_kg     DECIMAL
 );
 
+DAY 2,
 ALTER TABLE  animals 
 ADD COLUMN species VARCHAR(50)
+
+DAY 3,
+CREATE TABLE owners(
+  id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  full_name          VARCHAR(200),
+  age           INT
+);
+
+CREATE TABLE species(
+  id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name          VARCHAR(50)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE  animals 
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT Species_ID 
+FOREIGN KEY (species_id) 
+REFERENCES species (id);
+
+ALTER TABLE  animals 
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT Owners_ID 
+FOREIGN KEY (owner_id) 
+REFERENCES owners (id);
