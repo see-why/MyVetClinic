d58ad6da77d59161a0a012331ec86b4435f7f9ba
Iyadi Cyril <iyadicy@gmail.com>
2021-12-07 14:11:38 +0100

    Add FK indexes for the foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -17,6 +17,8 @@ CREATE TABLE medical_histories(
 	  REFERENCES patients(id)   
 );
 
+CREATE INDEX medical_histories_patient_id ON medical_histories(patient_id);
+
 CREATE TABLE treatments(
   id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   type          VARCHAR(50),
@@ -37,6 +39,9 @@ CREATE TABLE medical_histories_treatments(
 	  REFERENCES treatments(id)  
 );
 
+CREATE INDEX medical_histories_treatments_treatments_id ON medical_histories_treatments(treatments_id);
+CREATE INDEX medical_histories_treatments_medical_history_id ON medical_histories_treatments(medical_history_id);
+
 CREATE TABLE invoices(
   id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   total_amount          decimal,
@@ -50,6 +55,8 @@ CREATE TABLE invoices(
 
 );
 
+CREATE INDEX invoices_medical_history_id ON invoices(medical_history_id);
+
 CREATE TABLE invoice_items(
   id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   unit_price    decimal,
@@ -61,3 +68,5 @@ CREATE TABLE invoice_items(
       FOREIGN KEY(invoice_id) 
 	  REFERENCES invoices(id)  
 );
+
+CREATE INDEX invoice_items_invoice_id ON invoice_items(invoice_id);
